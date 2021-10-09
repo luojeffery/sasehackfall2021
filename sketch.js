0f8895eaa4e3e445143ba1b662022acdedc131b1
luojeffery <67873222+luojeffery@users.noreply.github.com>
2021-10-09 14:28:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// Your code will go here
+
+// Open up your console - if everything loaded properly you should see the version number 
+// corresponding to the latest version of ml5 printed to the console and in the p5.js canvas.
+function setup(){
+	createCanvas(400, 400);
+	textSize(width / 3);
+	textAlign(CENTER, CENTER);
+}
+
+function draw(){
+	background(200);
+	text(ml5.version, width/2, height/2);
+}
